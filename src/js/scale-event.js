0717b528644e0f319895d4a733db58f5fdccbaec
yaji2k <yaji2k@gmail.com>
2018-10-09 14:24:45 +0300

    edit

--- src/js/scale-event.js
+++ src/js/scale-event.js
@@ -1,9 +1,29 @@
 window.onload = function () {
-    var scaleActive = $(".app-event");
-    setWidth(1);
+    var scaleActive = $(".scale_state .scale_state_app-event");
+    var scalePoints = $(".scale_state .scale_state_app-event .scale_state_col_item");
+    var state = 2;
+    scaleState(state);
+
     function setWidth(count) {
-        var count = count || scaleActive.length - 1
-        $(".scale_active").css({"width": count / (scaleActive.length - 1) * 100 + "%"});
+        var count = count || 0;
+        var maxCount = scaleActive.length - 1;
+        if (count > maxCount) {
+            count = maxCount;
+        }
+        $(".scale_state_scale_active").css({"width": count / (scaleActive.length - 1) * 100 + "%"});
+    }
+
+    function setVisible(count) {
+        for (var i = 0; i <= count; i++) {
+            $(scalePoints[i]).css({"display": "block", "z-index": "initial"});
+        }
+    }
+
+    function scaleState(state) {
+        var count = (state === undefined) ? 0 : state;
+        console.log(count);
+        setWidth(count);
+        setVisible(count);
     }
 }
 
